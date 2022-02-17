57b3601b2cbae7baec027266e5655a0517304bad
ahmednayeeme <ahmednaim353@gmail.com>
2022-02-17 22:46:50 +0600

    saving-functionality

--- js/app.js
+++ js/app.js
@@ -1,29 +1,60 @@
-document.getElementById('calculate-button').addEventListener('click', function() {
+function inputValue(id) {
+
+    const input = document.getElementById(id);
+    return input
+
+}
 
 
-    const incomeInput = document.getElementById('income-field');
+
+
+
+
+// calculate handler
+inputValue('calculate-button').addEventListener('click', function() {
+
+
+    const incomeInput = inputValue('income-field');
     const incomeAmount = incomeInput.value;
 
 
-    const foodInput = document.getElementById('food-input');
+    const foodInput = inputValue('food-input');
     const foodAmount = foodInput.value;
 
 
-    const rentInput = document.getElementById('rent-input');
+    const rentInput = inputValue('rent-input');
     const rentAmount = rentInput.value;
 
-    const clothesInput = document.getElementById('clothes-input');
+    const clothesInput = inputValue('clothes-input');
     const clothesAmount = clothesInput.value;
 
     const expenses = parseFloat(foodAmount) + parseFloat(rentAmount) + parseFloat(clothesAmount)
 
-    const newExpenses = document.getElementById('total-cost')
+    const newExpenses = inputValue('total-cost')
     newExpenses.innerText = expenses;
 
-    const totalBalance = document.getElementById('total-balance');
-    const totalBalanceText = totalBalance.innerText;
-    const updateTotalBalance = parseFloat(totalBalanceText);
+    const totalBalance = inputValue('total-balance');
     totalBalance.innerText = incomeAmount - expenses;
+
+
+
+
+
+
+})
+
+
+// save handler
+inputValue('save-button').addEventListener('click', function() {
+
+    const incomeInput = inputValue('income-field').value;
+    const saveInput = inputValue('save-input').value;
+    const balance = inputValue('total-balance').innerText;
+    const calculate = saveInput / 100;
+    const saving = incomeInput * calculate;
+    const remainingBalance = balance - saving;
+    inputValue('total-saving').innerText = saving;
+    inputValue('total-remaining').innerText = remainingBalance;
 
 
 })
